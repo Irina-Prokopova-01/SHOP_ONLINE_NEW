a17b873615b15a74bc93adab9a7ba9f504e49e84
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-11-13 14:02:16 +0300

    add 123

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,8 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
-    path('register/', UserCreateView.as_view(template_name="register.html"), name='register'),
+    path('register/', UserCreateView.as_view(template_name="users/register.html"), name='register'),
     path('email-confirm/<str:token>', email_verification, name='email-confirm')
 ]
